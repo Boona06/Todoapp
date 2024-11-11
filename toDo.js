6db7a2d87380268ff565275833c71d4028d44215
bb851913@gmail.com <24LP7757@G302-CC0181.local>
2024-11-11 17:46:22 +0800

    to Do list

---
+++ toDo.js
@@ -0,0 +1,12 @@
+function addTask() {
+  const createOne = prompt("Даалгавар нэмээрэй");
+  document.getElementById("newTask").innerHTML +=
+    '<div id="newTask">' + createOne + "</div>";
+}
+addTask();
+// function deleteBtn(){
+//   const deletebox=[]
+//   for(let i=0 ; i<=){
+
+//   }
+// }
